f028e658e9ddab6af8f9439f28daa607c1ffecbe
Andrey Lapchik <a.lapchik7@gmail.com>
2023-06-28 12:44:47 +0300

    timer

--- client/src/pages/lobby/modules/chess/components/Game info/components/Time/BlackTimer.tsx
+++ client/src/pages/lobby/modules/chess/components/Game info/components/Time/BlackTimer.tsx
@@ -1,30 +1,10 @@
-import { useEffect, useState } from 'react'
 import { useAppSelector } from 'redux/store'
 import formatTime from './helpers/formatTime'
-import useGetBlackTime from './hooks/useGetBlackTime'
+import useBlackTimer from './hooks/useBlackTimer'
 
 const BlackTimer = () => {
-    const initialValue = useGetBlackTime()
-
-    const [time, setTime] = useState(initialValue)
+    const time = useBlackTimer()
     const { limit } = useAppSelector((store) => store.chess.time)
-    const {
-        turn,
-        gameStatus: { winner, draw }
-    } = useAppSelector((store) => store.chess).chessBoard
-
-    const isActive = turn === 'b'
-
-    // NEED TO STOP TIMER WHEN NECCESSARY
-
-    useEffect(() => {
-        if (initialValue && isActive && !(winner || draw)) {
-            const interval = setInterval(() => setTime(initialValue - 1), 1)
-            return () => clearInterval(interval)
-        } else if (initialValue) {
-            setTime(initialValue)
-        }
-    }, [initialValue, isActive, winner, draw])
 
     return (
         <div className='flex h-8 w-20 items-center justify-center rounded-md bg-white bg-opacity-5 text-xl font-semibold'>
--- client/src/pages/lobby/modules/chess/components/Game info/components/Time/WhiteTimer.tsx
+++ client/src/pages/lobby/modules/chess/components/Game info/components/Time/WhiteTimer.tsx
@@ -1,30 +1,10 @@
-import { useEffect, useState } from 'react'
 import { useAppSelector } from 'redux/store'
 import formatTime from './helpers/formatTime'
-import useGetWhiteTime from './hooks/useGetWhiteTime'
+import useWhiteTimer from './hooks/useWhiteTimer'
 
 const WhiteTimer = () => {
-    const initialValue = useGetWhiteTime()
-
-    const [time, setTime] = useState<number | null>(null)
+    const time = useWhiteTimer()
     const { limit } = useAppSelector((store) => store.chess.time)
-    const {
-        turn,
-        gameStatus: { winner, draw }
-    } = useAppSelector((store) => store.chess).chessBoard
-
-    const isActive = turn === 'w'
-
-    // NEED TO STOP TIMER WHEN NECCESSARY
-
-    useEffect(() => {
-        if (initialValue && isActive && !(winner || draw)) {
-            const interval = setInterval(() => setTime(initialValue - 1), 1)
-            return () => clearInterval(interval)
-        } else if (initialValue) {
-            setTime(initialValue)
-        }
-    }, [initialValue, isActive, winner, draw])
 
     return (
         <div className='flex h-8 w-20 items-center justify-center rounded-md bg-white bg-opacity-5 text-xl font-semibold'>
