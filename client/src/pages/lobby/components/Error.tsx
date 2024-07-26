c50a987a48f0bdb4f15a09bb03761e63b98e4f53
Andryshka16 <a.lapchik7@gmail.com>
2024-07-26 17:02:06 +0300

    added Docker

--- client/src/pages/lobby/components/Error.tsx
+++ client/src/pages/lobby/components/Error.tsx
@@ -6,14 +6,14 @@ import { useAppDispatch } from 'redux/store'
 const Error = () => {
 	const dispatch = useAppDispatch()
 	return (
-		<div className="mx-auto mt-4 flex h-fit max-h-[636px] w-full items-center justify-center rounded-lg bg-black bg-opacity-10 p-2.5 lg:w-[1000px]">
+		<div className='mx-auto mt-4 flex h-fit max-h-[636px] w-full items-center justify-center rounded-lg bg-black bg-opacity-10 p-2.5 lg:w-[1000px]'>
 			<div>
-				<img src={error} className="mx-auto h-40 w-40" />
-				<h1 className="mx-auto mb-16 mt-10 w-fit text-center text-4xl font-semibold leading-normal">
+				<img src={error} className='mx-auto mt-5 h-40 w-40' />
+				<h1 className='mx-auto mb-16 mt-10 w-fit text-center text-4xl font-semibold leading-normal'>
 					Error while fetching games <br /> Please try again later.
 				</h1>
 				<button
-					className="mx-auto block rounded-md bg-green-600 px-10 py-2 text-xl font-semibold transition duration-200 hover:bg-opacity-90"
+					className='mx-auto mb-10 block rounded-md bg-green-600 px-10 py-2 text-xl font-semibold transition duration-200 hover:bg-opacity-90'
 					onClick={() => {
 						dispatch(fetchRooms())
 						dispatch(fetchGame())
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,9 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
 	plugins: [react(), tsconfigPaths()],
-	optimizeDeps: {
-		include: ['config.json']
+	server: {
+		host: true,
+		strictPort: true,
+		port: 3000
 	}
 })
